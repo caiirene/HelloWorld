ab34bde7dcb17c4d9ce097aa20c9a004eecc0f20
不是饿饿，就是涩涩 <AAQQII901@GMAIL.COM>
2023-01-17 15:03:42 -0800

    2023 Jan17 15:03

---
+++ src/Book.java
@@ -0,0 +1,5 @@
+public class Book {
+    private String title;
+    private Person author;
+    private float price;
+}
---
+++ src/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int yearOfBirth;
+
+    public Person(String firstName, String lastName, int yearOfBirth){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.yearOfBirth = yearOfBirth;
+    }
+}
