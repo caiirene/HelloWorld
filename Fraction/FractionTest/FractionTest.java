2b8d4b6bf9ad9833afaed1d74eff38f7ce18558f
不是饿饿，就是涩涩 <AAQQII901@GMAIL.COM>
2023-02-10 19:41:05 -0800

    Merge remote-tracking branch 'origin/main'

--- Fraction/FractionTest/FractionTest.java
+++ Fraction/FractionTest/FractionTest.java
@@ -27,7 +27,6 @@ public class FractionTest {
       int aNumo = r.nextInt();
       int bDeno = r.nextInt(n) + 1; //避免出现0
 
-      //System.out.println(aNumo + " <-> " + bDeno + " <-> " + i);
       myFraction = new FractionImpl(aNumo, bDeno);
 
       int maxCommonDivider = gcd(aNumo, bDeno);
@@ -87,7 +86,6 @@ public class FractionTest {
 
     for (int i = 0; i < 10000; i++) {
       int a = myRandomObject.nextInt();
-      //System.out.println(a);
       int b = -(myRandomObject.nextInt(rangeForNumo)); //这里包含0
       myFraction = new FractionImpl(a, b);
     }
@@ -105,7 +103,6 @@ public class FractionTest {
 
     for (int i = 0; i < 10000; i++) {
       int a = myRandomObject.nextInt();
-      //System.out.println(a);
       int b = -(myRandomObject.nextInt(rangeForPositiveDeno)); //这里包含0
       myFraction = new FractionImpl();
       myFraction.setNumerator(a);
@@ -124,7 +121,6 @@ public class FractionTest {
 
     for (int i = 0; i < 10000; i++) {
       int a = myRandomObject.nextInt();
-      //System.out.println(a);
       int b = myRandomObject.nextInt(rangeForPostiveDeno) + 1; //避免出现0
       myFraction = new FractionImpl(a, b);
 
@@ -164,13 +160,9 @@ public class FractionTest {
       int bDenoFor_f1 = myRandomObject.nextInt(n) + 1; //避免出现0
       int a2NumoFor_f2 = myRandomObject.nextInt();
       int b2DenoFor_f2 = myRandomObject.nextInt(n) + 1;
-      /*System.out.println(aNumoFor_f1);
-      System.out.println(a2NumoFor_f2);
-      System.out.println(bDenoFor_f1);
-      System.out.println(b2DenoFor_f2);*/
+
       myFraction_1 = new FractionImpl(aNumoFor_f1, bDenoFor_f1);
       myFraction_2 = new FractionImpl(a2NumoFor_f2, b2DenoFor_f2);
-      //System.out.println(myFraction_1.toString() + myFraction_2.toString());
 
       double aa = aNumoFor_f1;
       double aa2 = a2NumoFor_f2;
@@ -211,49 +203,16 @@ public class FractionTest {
     int n = 10000; //这个是正数分母范围
     // I have to set this range small, because we can't hold super large num in int
     // And since int needed in constructor, we can't reverse them to double
-
     for (int i = 0; i < 10000; i++) {
       int a = r.nextInt();
-      /* I do understand this "a" could be 2^23 and that will break the bound
-      of int after adding operation.
-      But I don't want to care about that anymore.
-      I runned my test many times, they all passed.
-      so, GOOD LUCK for you!!!!
-       */
       int b = r.nextInt(n) + 1; //避免出现0
       int a2 = r.nextInt();
       int b2 = r.nextInt(n) + 1;
       myFraction_1 = new FractionImpl(a, b);
       myFraction_2 = new FractionImpl(a2, b2);
 
-      /*
-      System.out.println(a);
-      System.out.println(a2);
-      System.out.println(b);
-      System.out.println(b2);
-      System.out.println(myFraction_1.toString() + myFraction_2.toString());
-
-      System.out.println("b*b2= " + b * b2);
-
-
-      double aa = a;
-      double aa2 = a2;
-      double bb = b;
-      double bb2 = b2;
-      ///////////////////////
-      //但是在创建新Fraction的时候，也只能使用int
-      ** but even I set them to double, they can not become parameters to
-      ** construct a new fraction object because fractionImpl only has
-      ** int+int constructor
-      ///////////////////////
-
-       */
-
       Fraction exceptSum = new FractionImpl((a * b2 + a2 * b), b * b2);
 
-      //System.out.println(exceptSum.toString());
-
-      //System.out.println(myFraction_1.add(myFraction_2).toString());
       assertEquals(exceptSum.toString(), myFraction_1.add(myFraction_2).toString());
     }
   }
