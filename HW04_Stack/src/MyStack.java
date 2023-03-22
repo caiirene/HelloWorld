282694b3a8f1f6cb4c5295b0a7db51980d8c0e77
不是饿饿，就是涩涩 <AAQQII901@GMAIL.COM>
2023-03-22 10:59:48 -0700

    Merge remote-tracking branch 'origin/main'

--- HW04_Stack/src/MyStack.java
+++ HW04_Stack/src/MyStack.java
@@ -1,3 +1,72 @@
-public class MyStack {
+import java.util.ArrayList;
+import java.util.EmptyStackException;
 
+/**
+ * MyStack is a generic stack implementation using an ArrayList
+ * as the underlying data structure.
+ *
+ * @param <T> The type of elements stored in the stack.
+ */
+public class MyStack<T> implements MyStackInterface<T> {
+  private ArrayList<T> stack;
+
+  /**
+   * Constructs an empty MyStack instance.
+   */
+  public MyStack() {
+    stack = new ArrayList<>();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void push(T data) {
+    stack.add(data);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public T pop() {
+    if (empty()) {
+      throw new EmptyStackException();
+    }
+    return stack.remove(stack.size() - 1);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public T top() {
+    if (empty()) {
+      throw new EmptyStackException();
+    }
+    return stack.get(stack.size() - 1);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean empty() {
+    return stack.isEmpty();
+  }
+
+  /**
+   * Returns a string representation of the stack.
+   *
+   * @return A string containing "Stack: " followed by each element, separated by a single space.
+   */
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Stack: ");
+    for (T element : stack) {
+      sb.append(element).append(" ");
+    }
+    return sb.toString().trim();
+  }
 }
